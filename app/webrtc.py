0f07438460800a7edb41a35f1381b1ce2183180f
Ky Dao <ky.dao@fdssoft.com>
2024-06-28 02:41:35 +0700

    update code camera client

--- app/webrtc.py
+++ app/webrtc.py
@@ -1,14 +1,10 @@
-import os
 from aiortc import RTCPeerConnection, RTCSessionDescription, MediaStreamTrack
-from aiortc.contrib.media import MediaPlayer, MediaRelay
-import platform
 
 import cv2
-from fastapi.responses import JSONResponse
+# from fastapi.responses import JSONResponse
 
 from app.utils import CustomVideoTrack
 
-filename = "2.mp4"
 relay = None
 webcam = None
 pcs = set()
@@ -31,28 +27,6 @@ class VideoTransformTrack(MediaStreamTrack):
             pcs.clear()
         return frame
     
-# def create_local_tracks(play_from, decode):
-#     global relay, webcam
-#     # options = {"framerate": "30", "video_size": "1280x720"}
-#     # options = {"framerate": "30", "video_size": "1280x720", "rtbufsize": "10000000"}
-#     # options = {"framerate": "30", "video_size": "1280x720", "rtbufsize": "10000000", "pix_fmt": "yuv420p"}
-#     options = {"framerate": "30", "video_size": "960x540"}
-#     options = {"framerate": "30", "video_size": "640x360"}
-
-#     if play_from:
-#         player = MediaPlayer(play_from, decode=decode, options=options)
-#         return player.audio, player.video
-#     else:
-#         if relay is None:
-#             if platform.system() == "Darwin":
-#                 webcam = MediaPlayer("default:none", format="avfoundation", options=options)
-#             elif platform.system() == "Windows":
-#                 webcam = MediaPlayer("video=Logi C270 HD WebCam", format="dshow", options=options)
-#             else:
-#                 webcam = MediaPlayer("/dev/video0", format="v4l2", options=options)
-#             relay = MediaRelay()
-#         return None, relay.subscribe(webcam.video)
-
 async def handle_offer(request):
     params = await request.json()
     offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
@@ -76,15 +50,6 @@ async def handle_offer(request):
                 pc.addTrack(VideoTransformTrack(track))
         else:
             print("Track is not live.")
-        
-    # play_from = f"./data/{filename}"
-    # play_from = None
-    # decode = True
-    # audio, video = create_local_tracks(play_from=play_from, decode=decode)
-    # if video:
-    #     process_video = CustomVideoTrack(video, filename)
-    #     pc.addTrack(process_video)
-        
 
     await pc.setRemoteDescription(offer)
     answer = await pc.createAnswer()
